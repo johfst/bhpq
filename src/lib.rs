1025e0752abdec6e61137baac10b9b818409dfe9
johfst <jeremy@johfst.net>
2021-10-01 11:14:31 -0400

    added unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -127,3 +127,103 @@ impl fmt::Display for PriorityError {
 
 impl Error for PriorityError {}
 
+#[cfg(test)]
+mod tests {
+    use crate::BHPQ;
+
+    #[test]
+    fn new() {
+        let _bhpq: BHPQ<i32> = BHPQ::new(5);
+    }
+
+    #[test]
+    fn push() {
+        let mut bhpq: BHPQ<i32> = BHPQ::new(5);
+        bhpq.push(2, 25).unwrap();
+        assert!(bhpq.array[2].contains(&25));
+    }
+
+    #[test]
+    #[should_panic]
+    fn push_priorityerror() {
+        let mut bhpq: BHPQ<i32> = BHPQ::new(2);
+        bhpq.push(6, 65).unwrap();
+    }
+
+    #[test]
+    fn pop_value() {
+        let mut bhpq: BHPQ<i32> = BHPQ::new(2);
+        bhpq.push(1, 15).unwrap();
+        assert_eq!(bhpq.pop().unwrap(), 15);
+        assert!(bhpq.is_empty());
+    }
+
+    #[test]
+    #[should_panic]
+    fn pop_none() {
+        let mut bhpq: BHPQ<i32> = BHPQ::new(2);
+        bhpq.pop().unwrap();
+        bhpq.push(1, 15).unwrap();
+        bhpq.pop().unwrap();
+        bhpq.pop().unwrap();
+    }
+
+    #[test]
+    fn pop_order() {
+        let mut bhpq: BHPQ<i32> = BHPQ::new(3);
+        bhpq.push(2, 25).unwrap();
+        bhpq.push(1, 15).unwrap();
+        bhpq.push(0, 5).unwrap();
+        assert_eq!(bhpq.pop().unwrap(), 5);
+        assert_eq!(bhpq.pop().unwrap(), 15);
+        assert_eq!(bhpq.pop().unwrap(), 25);
+    }
+
+    #[test]
+    fn peek() {
+        let mut bhpq: BHPQ<i32> = BHPQ::new(2);
+        bhpq.push(1, 15).unwrap();
+        assert_eq!(*bhpq.peek().unwrap(), 15);
+    }
+
+    #[test]
+    fn peek_mut() {
+        let mut bhpq: BHPQ<i32> = BHPQ::new(2);
+        bhpq.push(1, 15).unwrap();
+        let top: &mut i32 = bhpq.peek_mut().unwrap();
+        *top = 16;
+        assert_eq!(*bhpq.peek().unwrap(), 16);
+    }
+
+    #[test]
+    fn is_empty() {
+        let bhpq: BHPQ<i32> = BHPQ::new(2);
+        assert!(bhpq.is_empty());
+    }
+
+    #[test]
+    fn clear() {
+        let mut bhpq: BHPQ<i32> = BHPQ::new(2);
+        bhpq.push(1, 15).unwrap();
+        bhpq.clear();
+        assert!(bhpq.is_empty());
+    }
+
+    #[test]
+    fn get_priority() {
+        let mut bhpq: BHPQ<i32> = BHPQ::new(2);
+        bhpq.push(1, 15).unwrap();
+        bhpq.get_priority(1).unwrap();
+    }
+
+    #[test]
+    fn get_priority_mut() {
+        use std::collections::LinkedList;
+        let mut bhpq: BHPQ<i32> = BHPQ::new(2);
+        bhpq.push(1, 15).unwrap();
+        let ll: &mut LinkedList<i32> = bhpq.get_priority_mut(1).unwrap();
+        let front = ll.front_mut().unwrap();
+        *front = 16;
+        assert_eq!(*bhpq.peek().unwrap(), 16);
+    }
+}
